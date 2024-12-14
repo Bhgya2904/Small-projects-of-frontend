b3017ef506268d82825a6e9cf6ce2cb512cb220f
Bhgya2904 <147989431+Bhgya2904@users.noreply.github.com>
2024-12-14 20:05:46 +0530

    temperature convertor
    this is a project based on changing temperature from celsius to fahrenheit and vice-versa.

---
+++ temp.js
@@ -0,0 +1,22 @@
+const textbox=document.getElementById("textbox");
+const tofarenheit=document.getElementById("tofarenheit");
+const tocelsius=document.getElementById("tocelsius");
+const result=document.getElementById("result");
+let temp;
+ function convert(){
+    if(tofarenheit.checked){
+        temp=Number(textbox.value);
+        temp=temp*9/5+32;
+        result.textContent=temp.toFixed(1)+"degrees";
+    }
+    else if(tocelsius.checked){
+        temp=Number(textbox.value);
+        temp=(temp-32)*(5/9);
+        result.textContent=temp.toFixed(1)+"degrees"
+
+    }
+    else{
+        result.textContent="Select a unit";
+    }
+
+}
